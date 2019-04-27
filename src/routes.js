0610e1970cba3a3d05fb37e26255cc5eb73bbe34
Sergio Rosa <scarlosjazz@gmail.com>
2019-04-27 18:42:13 -0400

    Scheduling Form

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,7 @@ const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
 const DashboardController = require('./app/controllers/DashboardController')
 const FileController = require('./app/controllers/FileController')
+const AppointmentController = require('./app/controllers/AppointmentController')
 
 routes.use((req, res, next) => {
   res.locals.flashSucces = req.flash('success')
@@ -31,5 +32,7 @@ routes.use('/app', authMiddleware)
 routes.get('/app/logout', SessionController.destroy)
 
 routes.get('/app/dashboard', DashboardController.index)
+
+routes.get('/app/appointments/new/:provider', AppointmentController.create)
 
 module.exports = routes
